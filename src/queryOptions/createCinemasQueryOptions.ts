8c74e15476faa0520b7f58d34295215efee71935
Гулевич Екатерина <meteorgul@gmail.com>
2025-10-14 11:39:25 +0300

    refactoring

--- src/queryOptions/createCinemasQueryOptions.ts
+++
@@ -1,8 +0,0 @@
-import { createQueryOptions } from './createQueryOptions.ts';
-
-export function createCinemasQueryOptions() {
-  return createQueryOptions<{ name: string }[]>({
-    key: ['cinemas'],
-    endpoint: '/cinemas',
-  });
-}
--- src/queryOptions/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './createCinemasQueryOptions.ts';
